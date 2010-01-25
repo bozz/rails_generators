41b35dc7c220425f44649d47c35279a3ebf956eb
bsearles <boris@lucidgardens.com>
2010-01-25 20:26:25 +0100

    initial commit

---
+++ lucid_layout/lucid_layout_generator.rb
@@ -0,0 +1,32 @@
+class LucidLayoutGenerator < Rails::Generator::Base
+  def initialize(runtime_args, runtime_options = {})
+    super
+    @name = @args.first || 'application'
+  end
+  
+  def manifest
+    record do |m|
+      m.directory 'app/views/layouts'
+      m.directory 'public/stylesheets'
+      m.directory 'app/helpers'
+     
+      m.template "layout.html.erb", "app/views/layouts/#{file_name}.html.erb"
+      m.file     "stylesheet.css",  "public/stylesheets/#{file_name}.css"
+      m.file "helper.rb", "app/helpers/layout_helper.rb"
+    end
+  end
+  
+  def file_name
+    @name.underscore
+  end
+
+  protected
+
+    def banner
+      <<-EOS
+Creates generic layout, stylesheet, and helper files.
+
+USAGE: #{$0} #{spec.name} [layout_name]
+EOS
+    end
+end
